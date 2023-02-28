37521ad883ed5bd4d9e7a0845124226f44c1534f
Mike Bastidas <mike.bastidas@hotmail.com>
2023-02-28 00:41:31 -0600

    did the reversing and inputting for the ids

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <stack>
 
 #include "ArgumentManager.h"
 
 using namespace std;
+
+string removeReversePar(string line, int length){       //ngl copied from leetcode solution XD
+    stack<char> stack;
+    string sorted;
+
+    for(int i = 0; i < line.length(); i++){
+        if(line.at(i) != ')'){
+            stack.push(line.at(i));
+        }else{
+            string temp;
+            while(!stack.empty() && stack.top() != '('){
+                temp += stack.top();
+                stack.pop();
+            }
+
+            if(!stack.empty() && stack.top() == '(')
+                stack.pop();
+
+            for(int x = 0; x < temp.length(); x++){
+                stack.push(temp.at(x));
+            }
+        }
+    }
+
+    while(!stack.empty()){
+        sorted += stack.top();
+        stack.pop();
+    }
+
+    reverse(sorted.begin(), sorted.end());
+
+    return sorted;
+}
 
 int main(int argc, char* argv[]){
 
@@ -15,10 +50,24 @@ int main(int argc, char* argv[]){
     ifstream input("input1.txt");
 
     string lineRead;
-    int counter;
+    int counter, numConv, parIndex;
 
-    while(getline(input, lineRead)){
+    while(getline(input, lineRead)){        //says only linked list is required for recursion
+        lineRead.erase(remove(lineRead.begin(), lineRead.end(), '\n'), lineRead.end());
+        lineRead.erase(remove(lineRead.begin(), lineRead.end(), '\r'), lineRead.end());
 
+        if(lineRead == "Bar1" || lineRead == "Bar2" || lineRead.empty()){
+            continue;
+        }else{
+            if(lineRead.find("(") >= 0 && lineRead.find(")") > 0){
+                lineRead = removeReversePar(lineRead, lineRead.length());
+                cout<< lineRead<<endl;
+            }else{
+                numConv = stoi(lineRead);
+                cout<< numConv<<endl;
+                //add it into the list itself as list.add(stoi(lineRead));
+            }
+        }
     }
 
     return 0;
